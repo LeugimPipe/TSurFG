b5a4473aa52f9fc8a94833207dedd379b0aaee09
LeugimPipe <pinarperezm@gmail.com>
2026-06-18 22:32:33 +0200

    Cámara en 3D bastante hecha: movimiento de zoom y orbital

--- project/view/3d/camera_gimbal/camera_gimbal.gd
+++ project/view/3d/camera_gimbal/camera_gimbal.gd
@@ -1,7 +1,11 @@
 extends Node3D
 
-@export var cam_speed = PI/2
+@export var orb_cam_speed = PI/2
+@export var tras_cam_speed = 1.5
 @export var zoom_speed = 0.05
+var zoom = 1.0
+var zoom_lower = 0.1
+var zoom_upper = 2.0
 var zoom_factor = 1.0 : set = set_zoom_factor
 
 func set_zoom_factor(new_factor : float) -> void:
@@ -9,43 +13,63 @@ func set_zoom_factor(new_factor : float) -> void:
 	var tw = create_tween().set_trans(Tween.TRANS_SINE)
 	tw.tween_property(self, "zoom", zoom_factor, 0.3)
 	await tw.finished
-	zoom_speed = 0.05 * zoom_factor
+	zoom_speed = 0.1 * zoom_factor
 	zoom_lower = 0.1 * zoom_factor
 	zoom_upper = 2.0 * zoom_factor
 
-var zoom = 1.0
-var zoom_lower = 0.1
-var zoom_upper = 2.0
+# Sceen movement
+# Will be written by the keyboard
+# And the mouse in certain cases
+var long_mov = 0.0 # Longitude (horizontal)
+var lat_mov = 0.0 # Latitude (vertical)
+
+# Camera mode
+enum {ORB, TRANS}
+var mode = ORB
 
 func _input(event):
 	if event.is_action_pressed("cam_zoom_in"):
-		var tw = create_tween().set_trans(Tween.TRANS_LINEAR)
-		tw.tween_property(self, "zoom", zoom-zoom_speed, 0.1)
-		await tw.finished
+		zoom -= zoom_speed
 	if event.is_action_pressed("cam_zoom_out"):
-		var tw = create_tween().set_trans(Tween.TRANS_LINEAR)
-		tw.tween_property(self, "zoom", zoom+zoom_speed, 0.1)
-		await tw.finished
+		zoom += zoom_speed
 	
 	if event.is_action_pressed("cam_reset"):
 		var tw = create_tween().set_parallel().set_trans(Tween.TRANS_SINE)
-		tw.tween_property($GimbalInner/Camera3D, "rotation", Vector3.ZERO, 0.3)
 		tw.tween_property(self, "rotation", Vector3.ZERO, 0.3)
 		tw.tween_property($GimbalInner, "rotation", Vector3.ZERO, 0.3)
 		await tw.finished
+	
+	if event.is_action_pressed("cam_mode"):
+		if mode == ORB: mode = TRANS
+		elif mode == TRANS: mode = ORB
+	
+	if mode == ORB and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		if event is InputEventMouseMotion:
+			long_mov = -event.relative.x * 0.5
+			if abs(long_mov) <= 1.0: long_mov = 0.0
+			lat_mov = -event.relative.y * 0.5
+			if abs(lat_mov) <= 1.0: lat_mov = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	zoom = clamp(zoom, zoom_lower, zoom_upper)
-	scale = Vector3.ONE * zoom
+	scale = lerp( scale, Vector3.ONE * zoom, 0.1)
 	
-	var roll = Input.get_axis("cam_roll_left", "cam_roll_right")
-	$GimbalInner/Camera3D.rotate_z(roll * cam_speed * delta)
-	$GimbalInner/Camera3D.rotation.z = clamp($GimbalInner/Camera3D.rotation.z, -PI/2, PI/2)
+	# Get movements from keyboard
+	if mode != ORB or !Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		long_mov = Input.get_axis("cam_west", "cam_east")
+		lat_mov = Input.get_axis("cam_north", "cam_south")
 	
-	var y = Input.get_axis("cam_orb_left", "cam_orb_right")
-	rotate_y(y * cam_speed * delta)
+	# Orbital mode
+	if mode == ORB:
+		rotate_y(long_mov * orb_cam_speed * delta)
+		$GimbalInner.rotate_x(lat_mov * orb_cam_speed * delta)
+		$GimbalInner.rotation.x = clamp($GimbalInner.rotation.x, -PI/2, PI/2)
 	
-	var x = Input.get_axis("cam_orb_up", "cam_orb_down")
-	$GimbalInner.rotate_x(x * cam_speed * delta)
-	$GimbalInner.rotation.x = clamp($GimbalInner.rotation.x, -PI/2, PI/2)
+	# Translation mode
+	if mode == TRANS:
+		translate_object_local(long_mov * tras_cam_speed * delta * $GimbalInner/Camera3D.transform.basis.x)
+		translate_object_local(lat_mov * tras_cam_speed * delta * $GimbalInner.transform.basis.z)
+		var vert_mov = Input.get_axis("cam_down", "cam_up")
+		translate_object_local(vert_mov * tras_cam_speed * delta * ($GimbalInner.transform * $GimbalInner/Camera3D.transform.basis.y))
+		
